7ddf8fd2ea489fe9520b09e01a0b95e8d412e794
trnhhoan <164651137+nhhoann@users.noreply.github.com>
2026-02-11 16:10:50 +0700

    DataTypeDemo

---
+++ DataTypeDemo.java
@@ -0,0 +1,18 @@
+package com.fpoly.lab1;
+
+/**
+ *
+ * @author tranthinhuhoan
+ * DataType
+ */ 
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        byte b2 = 20;
+        byte b = (byte) (b1 + b2);
+//        int b = b1 + b2;
+
+        float f = 2.3f;
+        long a = 3000000000L;
+    }
+}
